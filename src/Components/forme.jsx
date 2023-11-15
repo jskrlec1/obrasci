aed9d23098424f4913819f71ed1e8e9811cf4589
jskrlec1 <josip.skrlecvk@gmail.com>
2023-11-15 17:14:50 +0100

    forme

--- src/Components/forme.jsx
+++ src/Components/forme.jsx
@@ -1,36 +1,15 @@
-/*import React from "react";
-
-const UnosForme = () => {
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Ime:
-          <input name="name" onChange={handleChange} value={ime} />
-        </label>
-        <input type="submit" value="Potvrdi" />
-      </form>
-    </div>
-  );
-};
-
-export default UnosForme;*/
-
 import React, { useState } from "react";
 
 const UnosForme = () => {
-  // 1. Kreirajte useState varijablu za pohranu imena
   const [ime, setIme] = useState("");
   const [listaImena, setListaImena] = useState([]);
 
-  // 2. Kreirajte funkciju koja dodaje ime u listu
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Dodajte provjeru da se izbjegne dodavanje praznog imena
     if (ime.trim() !== "") {
       setListaImena([...listaImena, ime]);
-      setIme(""); // Očistite unos nakon dodavanja imena
+      setIme("");
     }
   };
 
@@ -48,7 +27,6 @@ const UnosForme = () => {
         <input type="submit" value="Potvrdi" />
       </form>
 
-      {/* Prikaz liste unesenih imena */}
       <ul>
         {listaImena.map((ime) => (
           <li key={crypto.randomUUID()}>{ime}</li>
